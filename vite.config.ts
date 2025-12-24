190f82dfe935ee9c6ba81224c802435a1ea5f2a7
jabberwocky238 <7176656@qq.com>
2025-12-23 21:52:22 -0500

    dev: 卧槽？

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,22 +12,24 @@ export default defineConfig({
     }),
   ],
   define: {
-    // 这一步非常重要：在构建时将全局的 Buffer 替换为 polyfill
     'global': 'globalThis',
   },
-  ssr: {
-    // 强制 Vite 处理这些 CommonJS 包，将其转换为 ESM
-    noExternal: ['buffer', '@solana/web3.js', '@solana/spl-token', '@coral-xyz/anchor'],
-  },
   optimizeDeps: {
-    // 强制预构建这些包
-    include: ['buffer'],
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
   },
   resolve: {
     alias: {
-      // 告诉 Vite 当代码中出现 Buffer 或 node:buffer 时，指向安装的 buffer 包
-      'buffer': 'buffer/',
-      'node:buffer': 'buffer/',
+      buffer: 'buffer/',
     },
   },
 })
